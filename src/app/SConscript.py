02f6a8de4685bd525308f81e0e282ea08ed2b074
Siann <siannhann@gmail.com>
2025-03-22 16:03:30 -0400

    add app build config

---
+++ src/app/SConscript.py
@@ -0,0 +1,37 @@
+
+from SCons.Script import (
+    Import,
+    Return,
+) 
+
+Import("env")
+
+link_flags = list(
+    filter(
+        lambda x: not x.startswith("-Wl,--script="),
+        env["LINKFLAGS"],
+    )
+)
+
+link_flags += ["-Wl,--script=ld/app.ld"]
+
+app = env.Program(
+        "app",
+        source=[
+            "vectors.S",
+            "app_main.c",
+            "crt0.S",
+        ],
+        LIBS = [],
+        LINKFLAGS=link_flags,
+)
+
+app_bin = env.ObjCopy(
+    "app.bin",
+    app,
+    OBJCOPYFLAGS="--output-target=binary",
+)
+
+app = env.Incbin("incbin_app.S", app_bin)
+
+Return("app")
