f86c939f681e67e62781024e48a2a7cb2da7feaa
Siann <siannhann@gmail.com>
2025-03-22 15:54:03 -0400

    check app version

--- src/main.c
+++ src/main.c
@@ -1,4 +1,23 @@
-int main () {
-	while(1);
+#include <stdio.h>
+#include "app/app_header.h"
+
+bool check_app_version(const app_header_t header) {
+	if (header.magic != APP_HEADER_MAGIC) {
+		return false;
+	}
+	if (header.version.major != APP_HEADER_VERSION_MAJOR) {
+		return false;
+	}
+	if (header.version.minor != APP_HEADER_VERSION_MINOR) {
+		return false;
+	}
+	return true;
+}
+
+int main() {
+	if(!(check_app_version(app_header))) {
+		while(1);
+	}
 	return 0;
 }
+
